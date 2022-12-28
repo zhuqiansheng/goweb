929f7a36f698185699c1e8ee68d58f5a55f189e4
zhuqiansheng <48715833+zhuqiansheng@users.noreply.github.com>
2022-12-28 20:03:52 +0800

    创建Core核心结构

---
+++ framework/core.go
@@ -0,0 +1,19 @@
+package framework
+
+import "net/http"
+
+// Core 框架核心结构
+type Core struct {
+
+}
+
+// NewCore 初始化框架核心结构
+func NewCore() *Core {
+	return &Core{}
+}
+
+// 框架核心结构实现Handler接口
+func (c *Core) ServeHTTP(response http.ResponseWriter,request *http.Request) {
+	// todo
+
+}
